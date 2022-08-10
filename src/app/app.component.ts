071ea3c5e4013c7614813a175519f3d19a63183c
Adrion10 <lacehila@gmail.com>
2022-08-10 18:03:29 +0200

    this.list.push({})

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todolist';
+  title = 'todo list';
+  list: any[] = [];
+  addTask(item: String) {
+    this.list.push({ id: this.list.length, name: item });
+    console.warn(this.list);
+  }
 }
